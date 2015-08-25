99edd3c838ebc0a9cdfad0b2fa10349367931fac
oddshou <oddshou@sina.com>
2015-08-25 19:41:34 +0800

    some thing for intercept event

---
+++ OddshouDemo/src/com/oddshou/testall/fragment/FragmentScrollView.java
@@ -0,0 +1,35 @@
+package com.oddshou.testall.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.oddshou.testall.R;
+
+public class FragmentScrollView extends Fragment {
+    private int mScrollViewBgColor;
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+    }
+    
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        View rootView = inflater.inflate(R.layout.scroll_view, null);
+        if (mScrollViewBgColor != 0) {
+            rootView.findViewById(R.id.btnContent).setBackgroundColor(mScrollViewBgColor);
+        }
+        return rootView;
+    }
+    
+    public void setColor(int color){
+        mScrollViewBgColor = color;
+    }
+
+}
--- OddshouDemo/src/com/oddshou/testall/scroll/TabIterceptActivity.java
+++ OddshouDemo/src/com/oddshou/testall/scroll/TabIterceptActivity.java
@@ -1,24 +1,89 @@
 package com.oddshou.testall.scroll;
 
-import com.oddshou.testall.R;
-import com.oddshou.testall.R.layout;
+import java.util.ArrayList;
+import java.util.Locale;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Menu;
-import android.view.MenuItem;
+
+import com.oddshou.testall.R;
+import com.oddshou.testall.fragment.FragmentScrollView;
 
 public class TabIterceptActivity extends Activity {
 
-	@Override
+    private FragmentScrollView mFragmentScrollView1;
+    private FragmentScrollView mFragmentScrollView2;
+	private ViewPager mViewPager;
+
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tab_itercept);
-		ViewPager viewPager = (ViewPager)findViewById(R.id.viewPager);
+		 mViewPager = (ViewPager)findViewById(R.id.viewPager);
+		 initViewPager();
+	}
+	private ArrayList<Fragment> mFragmentList = new ArrayList<Fragment>();
+	private void initViewPager(){
+	    if (mFragmentScrollView1 == null) {
+	        mFragmentScrollView1 = new FragmentScrollView();
+        }
+	    if (mFragmentScrollView2 == null) {
+	        mFragmentScrollView2 = new FragmentScrollView();
+	        mFragmentScrollView2.setColor(Color.YELLOW);
+	    }
+	    mFragmentList.clear();
+	    mFragmentList.add(mFragmentScrollView1);
+	    mFragmentList.add(mFragmentScrollView2);
+		mViewPager.setAdapter(new TabFragmentAdapter(getFragmentManager(), mFragmentList));
 	}
 	
-	private void initViewPager(){
-		
+	/**
+	 * v4 包 的 FragmentPagerAdapter 不支持 android.app.FragmentManager，<p>
+	 * 需要使用FragmentActivity，并且使用getSupportFragmentManager()方法获取FragmentManager<p>
+	 * 但是 v13包支持
+	 * @author oddshou
+	 *
+	 */
+	public class TabFragmentAdapter extends FragmentPagerAdapter{
+	    private ArrayList<Fragment> mList;
+        public TabFragmentAdapter(FragmentManager fm, ArrayList<Fragment> list) {
+            super(fm);
+            // TODO Auto-generated constructor stub
+            mList = list;
+        }
+
+        @Override
+        public android.app.Fragment getItem(int arg0) {
+            // TODO Auto-generated method stub
+            if (mList.size() > 0) {
+                return mList.get(arg0);
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return mList.size();
+        }
+        
+        @Override
+        public CharSequence getPageTitle(int position) {
+//            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return "page0";
+                case 1:
+                    return "page1";
+            }
+            return null;
+        }
+	    
 	}
 }
